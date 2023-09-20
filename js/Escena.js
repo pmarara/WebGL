422c66fc92928c1ee566b6357043c27d9acd2f6c
pmarara <pmarara.upv.edu.es>
2023-09-20 19:13:05 +0200

    escena de ejemplo con three

--- js/Escena.js
+++ js/Escena.js
@@ -32,7 +32,7 @@ function init(){
 
     // Escena
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0.0, 0.0, 0.2);
+    scene.background = new THREE.Color( 0.0, 0.2, 0.9);
 
     //Camara
     camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
@@ -44,15 +44,53 @@ function loadScene(){
     const material = new THREE.MeshBasicMaterial( {color:'yellow', wireframe: true});
 
     const geoCubo = new THREE.BoxGeometry( 2, 2, 2);
+    const geoEsfera = new THREE.SphereGeometry( 1, 10, 10);
 
     const cubo = new THREE.Mesh( geoCubo, material );
+    const esfera = new THREE.Mesh( geoEsfera, material );
 
-    scene.add(cubo);
+    esferaCubo = new THREE.Object3D();
 
+    cubo.position.set( 1, 0, 0);
+    esfera.position.set(-1, 0, 0);
+    esferaCubo.position.set(0, 1, 0);
+
+    esferaCubo.add(cubo);
+    esferaCubo.add(esfera);
+    scene.add(esferaCubo)
+
+
+    // Suelo
+    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10,10,10), material);
+    suelo.rotation.x = -Math.PI/2;
+    scene.add(suelo);
+
+    //Importar un modelo JSON
+    const loader = new THREE.ObjectLoader;
+    loader.load('models/soldado/soldado.json', 
+                function(objeto){
+                    cubo.add(objeto);
+                    objeto.position.set(0,1,0);
+                }
+    );
+
+    //Importar modelo en GLTF
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load('models/robota/scene.gltf',
+                    function(gltf){
+                       gltf.scene.position.y = 1;
+                       gltf.scene.rotation.y = -Math.PI/2;
+                       gltf.scene.scale.set (1.2, 1.2, 1.2);
+                       esfera.add( gltf.scene);
+                    }
+    );
+
+    scene.add( new THREE.AxisHelper(5));
 }
 
 function update(){
-
+    angulo += 0.01;
+    esferaCubo.rotation.y = ( angulo);
 }
 
 function render(){
